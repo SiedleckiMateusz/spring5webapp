455af3195338328cea0b76d12f0d635bffcb29a8
Mateusz <siedleckimateus@gmail.com>
2020-03-02 14:39:12 +0100

    Add Author&Book domains, repository

---
+++ src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
@@ -0,0 +1,11 @@
+package guru.springframework.spring5webapp.bootstrap;
+
+import org.springframework.boot.CommandLineRunner;
+
+public class BootStrapData implements CommandLineRunner {
+
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
+}
